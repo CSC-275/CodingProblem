713a420335fa3a109eda3f34ebace2f958ec6fab
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-08-08 13:36:17 -0400

    Initial Commit

--- Game.h
+++ Game.h
@@ -6,14 +6,10 @@
 
 class Game {
 private:
-    KenoNumbers m_keno;
-    Player m_player;
-    std::vector<int> m_matches;
+  
 public:
-    Game();
-    void printMatches(std::vector<int>playerPicks,std::vector<int>kenoNumbers);
-    void displayResults();
-    void playGame();
+
 };
 
 #endif //GAME_H
+
